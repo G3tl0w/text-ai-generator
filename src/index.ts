89019d71df057ff4960850c75ab3c44eaaf972e9
G3tl0w <xingjie_wang@yahoo.com.sg>
2025-03-04 15:39:32 +0800

    Update index.ts
    Update prompt

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export default {
   async fetch(request, env) {
     const inputs = {
-      prompt: "cyberpunk cat",
+      prompt: "Cat cating burger",
     };
 
     const response = await env.AI.run(
